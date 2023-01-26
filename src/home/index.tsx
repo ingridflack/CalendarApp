ec31ade90882a50489ad1f5e61ff45c194a1186d
Ingrid Flack <ingrid_flack@hotmail.com>
2023-01-26 12:22:57 -0300

    Initial setup

---
+++ src/home/index.tsx
@@ -0,0 +1,32 @@
+import { globalStyles } from '@/styles/global'
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+
+import appPreview from '../assets/appPreview.svg'
+import Image from 'next/image'
+
+globalStyles()
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="lg">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={appPreview}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  )
+}
---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
